e5900485d679312ef1cceb65efde31fa727dc57e
Shafin The Developer <sahariar350@gmail.com>
2021-04-12 21:43:25 +0600

    something is cooking

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 5000;
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config()
+app.use(cors())
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+
+const uri = `mongodb+srv://${ process.env.DB_USER }:${ process.env.DB_PASS }@cluster0.gopg3.mongodb.net/${ process.env.DB_NAME }?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const bookings = client.db(`${ process.env.DB_NAME }`).collection("booking");
+
+    app.post('/booking', (req, res) => {
+        bookings.insertOne(req.body)
+            .then(response => console.log(response))
+    })
+
+});
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${ port }`)
